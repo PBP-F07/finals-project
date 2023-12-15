7c6c64a5e27d99965819142fa28ce17e4092fb71
dienzahraaa <dienzahra312@gmail.com>
2023-12-15 12:17:25 +0700

    functionality wishlist

--- literaphile/lib/screens/login.dart
+++ literaphile/lib/screens/login.dart
@@ -43,58 +43,6 @@ class _LoginPageState extends State<LoginPage> {
             appBar: AppBar(
                 title: const Text('Login'),
             ),
-<<<<<<< HEAD
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () async {
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-
-                // Cek kredensial
-                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                // Untuk menyambungkan Android emulator dengan Django pada localhost,
-                // gunakan URL http://10.0.2.2/
-                final response =
-                    await request.login("https://literaphile-f07-tk.pbp.cs.ui.ac.id/login/", {
-                  'username': username,
-                  'password': password,
-                });
-
-                if (request.loggedIn) {
-                  String message = response['message'];
-                  String uname = response['username'];
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyHomePage()),
-                  );
-                  // ignore: use_build_context_synchronously
-                  ScaffoldMessenger.of(context)
-                    ..hideCurrentSnackBar()
-                    ..showSnackBar(SnackBar(
-                        content: Text("$message Selamat datang, $uname.")));
-                } else {
-                  // ignore: use_build_context_synchronously
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('Login Gagal'),
-                      content: Text(response['message']),
-                      actions: [
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-=======
             body: Container(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
@@ -105,7 +53,6 @@ class _LoginPageState extends State<LoginPage> {
                             decoration: const InputDecoration(
                                 labelText: 'Username',
                             ),
->>>>>>> dfd2d039d20f635a3d4e858b9b9a5f7be72e4b8d
                         ),
                         const SizedBox(height: 12.0),
                         TextField(
