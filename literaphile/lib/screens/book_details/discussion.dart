18b2346609a524432ac64b4ecce6b2cf4e3b6bad
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-12-17 17:09:13 +0700

    backup

--- literaphile/lib/screens/book_details/discussion.dart
+++ literaphile/lib/screens/book_details/discussion.dart
@@ -1,12 +1,12 @@
 import 'package:literaphile/models/discussion.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class DiscussionPage extends StatefulWidget {
   final int bookId;
 
-  const DiscussionPage({Key? key,required this.bookId}) : super(key: key);
+  const DiscussionPage({Key? key, required this.bookId}) : super(key: key);
 
   @override
   _DiscussionPageState createState() => _DiscussionPageState();
@@ -14,26 +14,24 @@ class DiscussionPage extends StatefulWidget {
 
 class _DiscussionPageState extends State<DiscussionPage> {
   late String id;
+  double fem = 1.0; // Define the 'fem' factor
 
   @override
   void initState() {
     super.initState();
-    // Accessing the bookId from the widget property and assigning it to the 'id' variable
     id = widget.bookId.toString();
   }
+
   Future<List<Discussion>> fetchProduct() async {
-    var urlAwal = 'http://localhost:8000/details/discussion/$id/json/' ;
-    var url = Uri.parse(
-        urlAwal);
+    var urlAwal = 'http://localhost:8000/details/discussion/$id/json/';
+    var url = Uri.parse(urlAwal);
     var response = await http.get(
       url,
       headers: {"Content-Type": "application/json"},
     );
 
-    // melakukan decode response menjadi bentuk json
     var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-    // melakukan konversi data json menjadi object Product
     List<Discussion> list_product = [];
     for (var d in data) {
       if (d != null) {
@@ -46,51 +44,94 @@ class _DiscussionPageState extends State<DiscussionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Product'),
-        ),
-        body: FutureBuilder(
-            future: fetchProduct(),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                if (!snapshot.hasData) {
-                  return const Column(
-                    children: [
-                      Text(
-                        "Tidak ada data produk.",
-                        style:
-                        TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+      appBar: AppBar(
+        title: const Text('Product'),
+      ),
+      backgroundColor: Color(0xffb4c5e4), // Set the background color here
+      body: Container(
+        padding: EdgeInsets.fromLTRB(17 * fem, 39 * fem, 17 * fem, 230 * fem),
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: fetchProduct(),
+                builder: (context, AsyncSnapshot<List<Discussion>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        "Error: ${snapshot.error}",
+                        style: TextStyle(color: Colors.red),
                       ),
-                      SizedBox(height: 8),
-                    ],
-                  );
-                } else {
-                  return ListView.builder(
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        "Tidak ada data diskusi.",
+                        style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
                       itemCount: snapshot.data!.length,
                       itemBuilder: (_, index) => Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 12),
-                        padding: const EdgeInsets.all(20.0),
+                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "${snapshot.data![index].fields.user}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                            Container(
+                              width: double.infinity,
+                              height: 87 * fem,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10 * fem),
+                                border: Border.all(color: Color(0xff3c3744)),
+                                color: Color(0xfffbfff1),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${snapshot.data![index].fields.user}",
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text("${snapshot.data![index].fields.comment}"),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 10),
-                            Text("${snapshot.data![index].fields.comment}"),
                           ],
                         ),
-                      ));
-                }
-              }
-            }));
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
